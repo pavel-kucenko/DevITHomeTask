6a81bf4e4cb9a4309d5c38215d033dec5630540f
Pavel Kucenko <smartflaber@gmail.com>
2021-07-18 18:22:59 +0300

    test js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,24 @@
-const app = document.querySelector("#app");
+const linkProducts = document.querySelectorAll(".link-products");
 
-
-
-
-
-const historyResolver = (title, location) => {
-    this.event.preventDefault();
-
-    history.pushState({}, title, location)
-
-    switch(location) {
-        case '/test.html':
-            console.log(app);
-            $("#app").load("./test.html"); 
-            break
-    }
+for (let i = 0; i < linkProducts.length; i++) {
+    linkProducts[i].addEventListener('click', (e) => {
+        e.preventDefault();
+        history.pushState({}, 'products', '/pages/test.html')
+        $("#head").remove();
+        $("#spa").load("./pages/test.html")
+    })
 }
+
+
+const linkHome = document.querySelectorAll(".link-home");
+
+console.log(linkHome);
+
+for (let i = 0; i < linkHome.length; i++) {
+    linkHome[i].addEventListener('click', (e) => {
+        e.preventDefault();
+        history.pushState({}, 'home', '/')
+        $("html").load("./index.html")
+    })
+}
+
